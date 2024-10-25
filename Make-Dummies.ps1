0e7b2d1bf2d2671daf50f018b4080d9f5ffc8293
ceresgalax <ceresgalax@gmail.com>
2024-10-25 00:51:40 -0700

    Remove Cubeb.targets from nuget package, change to NoTargets SDK to prevent an empty .NET assembly from being included.
    This fixes relative path errors when running dotnet publish on a project
    depending on this package.

---
+++ Make-Dummies.ps1
@@ -0,0 +1,5 @@
+# For testing local packaging without having the actual binaries built.
+New-Item osx-x64\libcubeb.dylib
+New-Item osx-arm64\libcubeb.dylib
+New-Item win-x64\cubeb.dll
+New-Item win-x64\cubeb.lib
